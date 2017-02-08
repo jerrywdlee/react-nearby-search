925f515200243b9f94e963add56f0815654aea5f
Jerry Lee <lzjjerrywoodylee@gmail.com>
2017-02-08 21:53:51 +0800

    Navbarの最終レイアウト

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import MyNavbar from './components/Navbar.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MyNavbar></MyNavbar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>React周辺施設検索</h2>
